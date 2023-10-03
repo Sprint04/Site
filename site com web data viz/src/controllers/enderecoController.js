924d6a80b529af82368688a6356c9a11799d0b49
EvertonSPTECH <everton.xavier@sptech.school>
2023-10-03 08:53:12 -0300

    Session Storage funcionando para efetuar a consulta e cadastro de endereço

--- site com web data viz/src/controllers/enderecoController.js
+++ site com web data viz/src/controllers/enderecoController.js
@@ -24,15 +24,35 @@ function autenticarEndereco(req, res) {
                     console.log(`\n Resultados encontrados: ${resultado.length}`);
                     console.log(`Resultados: ${JSON.stringify(resultado)}`) //transforma JSON em string
 
-                    if (resultado == 1) {
+                    if (resultado.length == 1) {
                         console.log(resultado);
                         res.json(resultado[0]);
                     } else if (resultado.length == 0) {
-                        res.status(403).send("CEP Não encontrado, iniciando cadastro")
+                        console.log("CEP Não encontrado, iniciando cadastro")
                         enderecoModel.cadastrarEndereco(cep, estado, cidade, bairro, rua)
                             .then(
                                 function (resultado) {
                                     res.json(resultado);
+                                    console.log(resultado)
+
+                                    enderecoModel.autenticarEndereco(cep)
+                                        .then(
+                                            function (resultado) {
+                                                console.log(`\n Resultados encontrados: ${resultado.length}`);
+                                                console.log(`Resultados: ${JSON.stringify(resultado)}`) //transforma JSON em string
+
+                                                if (resultado.length == 1) {
+                                                    console.log(resultado);
+                                                    res.json(resultado[0]);
+                                                }
+                                            }
+                                            ).catch(
+                                                function (erro) {
+                                                    console.log(erro);
+                                                    console.log("\nHouve um erro ao consultar endereço! Erro: ", erro.sqlMessage);
+                                                    res.status(500).json(erro.sqlMessage);
+                                                }
+                                            );
                                 }
                             ).catch(
                                 function (erro) {
@@ -44,25 +64,6 @@ function autenticarEndereco(req, res) {
                                     res.status(500).json(erro.sqlMessage);
                                 }
                             );
-                        enderecoModel.autenticarEndereco(cep)
-                            .then(
-                                function (resultado) {
-                                    console.log(`\n Resultados encontrados: ${resultado.length}`);
-                                    console.log(`Resultados: ${JSON.stringify(resultado)}`) //transforma JSON em string
-
-                                    if (resultado == 1) {
-                                        console.log(resultado);
-                                        res.json(resultado[0]);
-                                    }
-                                })
-                                .catch(
-                                    function (erro) {
-                                        console.log(erro);
-                                        console.log("\nHouve um erro ao consultar endereço! Erro: ", erro.sqlMessage);
-                                        res.status(500).json(erro.sqlMessage);
-                                    }
-                                );
-
                     } else {
                         res.status(403).send("Mais de um endereço encontrado!")
                     }
