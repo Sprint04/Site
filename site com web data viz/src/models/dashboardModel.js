afc22ac351a85c359fc9386fb34760450f98cdf5
cesarhatakemonkey <cesar.bmartins@sptech.school>
2023-10-20 11:29:25 -0300

    formatação da data!!!

--- site com web data viz/src/models/dashboardModel.js
+++ site com web data viz/src/models/dashboardModel.js
@@ -3,7 +3,8 @@ var database = require("../database/config")
 function buscarUltimasMedidas() {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ")
     var instrucao = `
-        SELECT dadoCapturado, dtHora FROM Monitoramento;
+    SELECT dadoCapturado, DATE_FORMAT(dtHora, '%d/%m/%Y às %HH') as dtHora FROM Monitoramento;
+    ;
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
