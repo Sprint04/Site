b7b6974e6e242ce72177e10669b2780e6ce2622b
cesarhatakemonkey <cesar.bmartins@sptech.school>
2023-10-25 16:35:41 -0300

    'gráfico-cpu-funcionando'

--- site com web data viz/src/models/dashboardModel.js
+++ site com web data viz/src/models/dashboardModel.js
@@ -3,7 +3,7 @@ var database = require("../database/config")
 function buscarUltimasMedidas() {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ")
     var instrucao = `
-    SELECT dadoCapturado, DATE_FORMAT(dtHora, '%d/%m/%Y às %HH') as dtHora FROM Monitoramento;
+    SELECT dadoCapturado, DATE_FORMAT(dtHora, '%d/%m/%Y às %HH') as dtHora FROM Monitoramento where fkComponente = '1';
     ;
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
