c673e7fe594669c23821ddd1696f5d5dc6984883
Nathan Ribeiro de Araujo Oliveira <nathan.oliveira@sptech.school>
2023-10-31 21:31:41 -0300

    criei o token

--- site com web data viz/src/models/empresaModel.js
+++ site com web data viz/src/models/empresaModel.js
@@ -25,11 +25,22 @@ function buscarPorCnpj(cnpj) {
 function cadastrarEmpresa(nome, cnpj, fkEnderecoComplemento) {
   console.log("ACESSEI O USUARIO MODEL \n \n \t \t >> Se aqui der erro de 'Error: connect ECONNREFUSED', \n \t \t >> verifique suas credencias de acesso ao banco \n \t \t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", nome, cnpj, fkEnderecoComplemento)
   var instrucao = `
-    insert into Empresa (idEmpresa, nome, CNPJ, fkEnderecoComplemento, fkPlano) values
-    (null, "${nome}", "${cnpj}", ${fkEnderecoComplemento}, 1);
+    insert into Empresa (idEmpresa, nome, CNPJ, fkEnderecoComplemento, fkPlano, fkToken) values
+    (null, "${nome}", "${cnpj}", ${fkEnderecoComplemento}, 1, (select max(idToken) from tokens));
     `
+  var token = ""
+  var op = ["1","2","3","4","5","6","7","8","9","a",'b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+  for (i = 0; token.length < 25; i++){
+    letra = Math.floor(Math.random() * 36)
+    token += op[letra]
+  }
+  var instrucao2 = `
+  insert into Tokens (chaveAtivacao, fkSituacao) value
+     ('${token}', 1)
+  `
 
   console.log("Executando a instrução SQL: \n" + instrucao);
+  database.executar(instrucao2);
   return database.executar(instrucao);
 }
 
